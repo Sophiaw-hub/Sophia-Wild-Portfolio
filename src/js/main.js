232c22f0bd87e8bf8506491e63a423214276c27d
DESKTOP-UUMLACG\sosow <sosowalld353@gmail.com>
2025-12-09 10:51:19 +0100

    add main content about

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import { initNavigation } from "./navigation.js";
-//import { initScrollHeader } from "./scrollHeader.js";
+import { initScrollHeader } from "./scrollHeader.js";
+import { initCircleBorder } from "./circleBorder.js";
 
 initNavigation();
-//initScrollHeader();
+initScrollHeader();
+initCircleBorder()
--- src/js/scrollHeader.js
+++ src/js/scrollHeader.js
@@ -2,7 +2,7 @@ export function initScrollHeader() {
   const header = document.querySelector("header");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 10) {
+    if (window.scrollY > 10 && window.innerWidth<576) {
       header.classList.add("scrolled");
     } else {
       header.classList.remove("scrolled");
